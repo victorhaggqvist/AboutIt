41930f5080c0f46270acb39979bb02f964c3346d
Victor Häggqvist <victor@snilius.com>
2015-09-17 03:20:28 +0200

    add skeleton for creating library presets

---
+++ aboutit/src/main/java/com/snilius/aboutit/LibSkeleton.java
@@ -0,0 +1,12 @@
+package com.snilius.aboutit;
+
+/**
+ * Library skeleton for creation of presets
+ * @author Victor Häggqvist
+ * @since 9/17/15
+ */
+public class LibSkeleton extends AboutIt.Lib {
+    public LibSkeleton(String name, String author, LicenseBase license, String url) {
+        super(name, author, license, url);
+    }
+}
