1a9d15cd30dae36e2e518a8ee5957d38e79f3d6c
Victor Häggqvist <victor@snilius.com>
2015-09-17 03:48:21 +0200

    add aboutit-meta presets module

---
+++ aboutit-meta/src/main/java/com/snilius/aboutit/meta/LibGlide.java
@@ -0,0 +1,14 @@
+package com.snilius.aboutit.meta;
+
+import com.snilius.aboutit.CustomLicense;
+import com.snilius.aboutit.LibSkeleton;
+
+/**
+ * @author Victor Häggqvist
+ * @since 9/17/15
+ */
+public class LibGlide extends LibSkeleton {
+    public LibGlide() {
+        super("Glide", "Sam Judd", new CustomLicense("BSD, part MIT and Apache 2.0"), "https://github.com/bumptech/glide");
+    }
+}
---
+++ aboutit-meta/src/main/java/com/snilius/aboutit/meta/LibLeakCanary.java
@@ -0,0 +1,14 @@
+package com.snilius.aboutit.meta;
+
+import com.snilius.aboutit.L;
+import com.snilius.aboutit.LibSkeleton;
+
+/**
+ * @author Victor Häggqvist
+ * @since 9/17/15
+ */
+public class LibLeakCanary extends LibSkeleton {
+    public LibLeakCanary() {
+        super("LeakCanary", "Square, Inc.", L.AP2, "https://github.com/square/leakcanary");
+    }
+}
