601148363b2df29d8a640f8502fd41ec5a49c165
Victor Häggqvist <victor@snilius.com>
2015-09-17 03:07:03 +0200

    add a bunch of licenses

--- aboutit/src/main/java/com/snilius/aboutit/L.java
+++ aboutit/src/main/java/com/snilius/aboutit/L.java
@@ -5,8 +5,10 @@ package com.snilius.aboutit;
  * @author Victor Häggqvist
  * @since 12/29/14
  */
-    AP2("Apache License, Version 2.0"), MIT("MIT"), GPL2("GPLv2"), GPL3("GPLv3");
 public enum L implements LicenseBase {
+    AP2("Apache License, Version 2.0"), MIT("MIT"),
+    GPL2("GPL v2.0"), GPL3("GPL v3.0"), AGPL3("Affero GPL v3.0"),
+    BSD("BSD"), SBSD("Simplified BSD"), NBSD("New BSD");
 
     private String mDisplayname;
 
